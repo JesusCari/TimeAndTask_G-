a01f9bd55e16e22b638fb843781d94616dc5b07b
Diego Lucas <72081775+thediegolucas@users.noreply.github.com>
2020-11-23 15:55:07 +0000

    Add files via upload

---
+++ CA2_TimeAndTask_GroupG.java
@@ -0,0 +1,13 @@
+
+package ca2_timeandtask_groupg;
+
+public class CA2_TimeAndTask_GroupG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");
+        System.out.println();
+        System.out.println("This is our project and every commit will be below...");
+        System.out.println();
+    }
+    
+}
